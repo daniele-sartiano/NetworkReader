af3221fd657c33177dd67efee46f75a909857c02
Daniele <daniele.sartiano@gmail.com>
2018-01-29 19:20:30 +0100

    class sample

--- main.cpp
+++ main.cpp
@@ -2,3 +2,27 @@
 // Created by daniele on 29/01/18.
 //
 
+#include <iostream>
+#include <string>
+#include <pcap.h>
+
+
+using namespace std;
+
+class PcapReader {
+    string filename;
+public:
+    explicit PcapReader (string f) : filename(f) {}
+    void read() {
+        cout << this->filename;
+    }
+};
+
+int main() {
+
+    PcapReader *reader = new PcapReader("afile");
+    reader->read();
+
+    std::cout << "Hello world\n";
+    return(0);
+}
