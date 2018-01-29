c939a2f2e6258d36f1e367005f03e6962e468a6a
daniele-sartiano <daniele.sartiano@gmail.com>
2018-01-29 21:21:14 +0100

    added getopt

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <pcap.h>
+#include <getopt.h>
 
 
 using namespace std;
@@ -13,14 +14,32 @@ class PcapReader {
     string filename;
 public:
     explicit PcapReader (string f) : filename(f) {}
+
     void read() {
-        cout << this->filename;
+        cout << this->filename << "\n";
     }
 };
 
-int main() {
+int main(int argc, char* argv[]) {
+    const char* nvalue = "";
+    int c ;
+    while( ( c = getopt (argc, argv, "f:") ) != -1 )
+    {
+        switch(c)
+        {
+            case 'f':
+                if(optarg) {
+                    nvalue = optarg;
+                }
+                else {
+                    cerr << "got a nullptr for optarg for -n\n" ;
+                    return(-1);
+                }
 
-    PcapReader *reader = new PcapReader("afile");
+        }
+    }
+
+    PcapReader *reader = new PcapReader(nvalue);
     reader->read();
 
     std::cout << "Hello world\n";
