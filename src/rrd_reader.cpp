f02041658b5416c40093e5c98d9420c6072abf5f
Daniele <daniele.sartiano@gmail.com>
2018-02-20 21:14:37 +0100

    removed useless code

--- src/rrd_reader.cpp
+++ src/rrd_reader.cpp
@@ -3,17 +3,20 @@
 //
 
 
-#include <step.h>
 #include "rrd_reader.h"
 
 int RrdReader::read(string filename) {
+    return 0;
+}
 
+
+int RrdReader::read(string filename, pair<vector<string>, vector<Step>>& results) {
     int status;
     unsigned long ds_cnt = 0;
     char **ds_name;
     unsigned long step = 1;
     rrd_value_t *data;
-    pair<vector<string>, list<Step>> *results = new pair<vector<string>, list<Step>>();
+    //pair<vector<string>, vector<Step>> *results = new pair<vector<string>, vector<Step>>();
 
     bool names_initialized = false;
 
@@ -27,54 +30,38 @@ int RrdReader::read(string filename) {
         return status;
     }
 
-    const unsigned long length = (end - start) / step;
+    //const unsigned long length = (end - start) / step;
 
     size_t i, j;
     long c = 0;
     for (time_t t=(start + step); t <= end; t += step) {
-        i = (size_t) (t-start) / (size_t) step;
-        double values[ds_cnt];
+        //i = (size_t) (t-start) / (size_t) step;
+        vector<double> values;
+        bool are_nan = true;
         for (j=0; j < ds_cnt; ++j) {
             if (!names_initialized) {
-                results->first.push_back(ds_name[j]);
+                results.first.push_back(ds_name[j]);
             }
             if (ds_name[j] >= 0) {
-                time_t timestamp = t;
-                double value = data[c];
-                //cout << ds_name[j] << "\t" << timestamp << "\t" << value << endl;
-                values[j] = value;
+                values.push_back(data[c]);
             }
-            //++data;
+            if (are_nan && !isnan(data[c])) {
+                are_nan = false;
+            }
             ++c;
         }
-        //cout << t << values[0] << "|" << values[1] << endl;
-        results->second.push_back(Step(static_cast<unsigned long>(t), values));
+        if (!are_nan) {
+            Step s = Step(ds_cnt, static_cast<unsigned long>(t), values);
+            results.second.push_back(s);
+        }
         if (!names_initialized) {
             names_initialized = true;
         }
     }
 
-    /*for (unsigned int column=0; column < ds_cnt; ++column) {
-        cout << ds_name[column] << endl;
-        for (unsigned int row=0; row < length; ++row) {
-            cout << row << "\t" << data[row*ds_cnt+column] << endl;
-        }
-    }*/
+    std::sort(results.second.begin(), results.second.end());
 
-
-    /*for (list<Step>::iterator it = results.second.begin(); it != results.second.end(); it++) {
-        Step s = (*it);
-        cout << s.getTimestamp() << " " << s.getData()[0] << "-" << s.getData()[1] << endl;
-    }*/
-
-    //cout << results.first.front() << endl;
-    cout << results->second.front().getTimestamp() << endl;
-    cout << results->second.back().getTimestamp() << endl;
-    cout << results->second.size() << endl;
-    //cout << status << endl;
-    //results->first.clear();
-    //results->second.clear();
     free(data);
     free(ds_name);
-    delete results;
+    return 0;
 }
